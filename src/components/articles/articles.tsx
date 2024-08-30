27f78e4d81beb5a7a856d5a273048105cbb0b777
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2024-08-30 09:57:07 -0300

    chore: remove console.log

--- src/components/articles/articles.tsx
+++ src/components/articles/articles.tsx
@@ -5,8 +5,6 @@ import { Message } from "@/components/shared/message";
 export function Articles() {
   const { data, isError, isLoading } = useNewsArticles();
 
-  console.log(data);
-
   if (isError) {
     return <Message content="Something went wrong. Please try again later." />;
   }
